ed24f43b9040bbac9c9ccb2663551013c502aca2
cogolow-723 <s22c1124wl@s.chibakoudai.jp>
2023-11-27 15:49:42 +0900

    test3

--- test.bash
+++ test.bash
@@ -1,6 +1,20 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Natsumi Mori
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	ret=1
+}
+
+ret=0
+
+### I/O TEST ###
 
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 15 ]
+[ "$ret" = 0 ] && echo OK
+
+exit $ret
 
